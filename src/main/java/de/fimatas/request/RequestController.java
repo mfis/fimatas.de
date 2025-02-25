8a0740986d4c18853a0941c468af8e504ceeaed1
mfis <fischermatthias1980@googlemail.com>
2025-02-25 21:36:21 +0100

    adresse

--- src/main/java/de/fimatas/request/RequestController.java
+++ src/main/java/de/fimatas/request/RequestController.java
@@ -34,39 +34,16 @@ public class RequestController {
         model.addAttribute("email", obfuscateEmail(MAIL_ADDRESS + "@" + MAIL_HOST));
     }
 
-    private String generateRandomComment() {
-        Random rand = new Random();
-        int length = 4 + rand.nextInt(5);
-        StringBuilder comment = new StringBuilder();
-
-        for (int i = 0; i < length; i++) {
-            char randomChar = (rand.nextBoolean())
-                    ? (char) ('0' + rand.nextInt(10))
-                    : (char) ('a' + rand.nextInt(26));
-            comment.append(randomChar);
-        }
-
-        return comment.toString();
-    }
-
     private String obfuscateEmail(@SuppressWarnings("SameParameterValue") String email) {
-
         var text = "E-Mail: " + email;
         StringBuilder obfuscatedEmail = new StringBuilder();
-        Random rand = new Random();
         for (int i = 0; i < text.length(); i++) {
             char c = text.charAt(i);
             String htmlEntity = "&#" + (int) c + ";";
             obfuscatedEmail.append(htmlEntity);
-            if (rand.nextInt(2) == 0) {
-                String randomComment = generateRandomComment();
-                obfuscatedEmail.append("<!-- ").append(randomComment).append(" -->");
-            }
-            if (rand.nextInt(2) == 0) {
-                obfuscatedEmail.append("<!--\n-->");
-            }
+            obfuscatedEmail.append("<!-- ").append("x").append(" -->");
+            obfuscatedEmail.append("<!--\n-->");
         }
-
         return obfuscatedEmail.toString();
     }
 }
