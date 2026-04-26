072facbbf0f5c8dba04fdea82939df0f053e2e1e
mfis <fischermatthias1980@googlemail.com>
2026-04-26 20:05:16 +0200

    springboot4

--- src/main/java/de/fimatas/application/FimatasDeApplication.java
+++ src/main/java/de/fimatas/application/FimatasDeApplication.java
@@ -1,13 +1,17 @@
 package de.fimatas.application;
 
+import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("de.fimatas")
+@CommonsLog
 public class FimatasDeApplication {
     public static void main(String[] args) {
+        log.info("Spring Boot Version: " + SpringBootVersion.getVersion());
         SpringApplication.run(FimatasDeApplication.class, args);
     }
 }
