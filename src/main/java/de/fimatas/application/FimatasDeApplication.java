c8a39d1976b9287a21c824bc4bfc8df178403783
mfis <fischermatthias1980@googlemail.com>
2026-05-14 14:54:21 +0200

    log bytecode version

--- src/main/java/de/fimatas/application/FimatasDeApplication.java
+++ src/main/java/de/fimatas/application/FimatasDeApplication.java
@@ -8,6 +8,9 @@ import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.io.InputStream;
+import java.lang.invoke.MethodHandles;
+
 @SpringBootApplication
 @ComponentScan("de.fimatas")
 @CommonsLog
@@ -16,7 +19,21 @@ public class FimatasDeApplication {
         SpringApplication app = new SpringApplication(FimatasDeApplication.class);
         app.addListeners((ApplicationListener<ApplicationStartedEvent>) event -> {
             log.info("Spring Boot Version: " + SpringBootVersion.getVersion());
+            logBytecodeVersion();
         });
         app.run(args);
+    }
+
+    private static void logBytecodeVersion() {
+        Class<?> c = MethodHandles.lookup().lookupClass();
+        try (InputStream in = c.getResourceAsStream(c.getSimpleName() + ".class")) {
+            if (in != null) {
+                log.info("Java Bytecode Version: " + (in.readAllBytes()[7] - 44));
+            } else {
+                log.warn("Java Bytecode Version: unknown");
+            }
+        } catch (Exception e) {
+            log.error("Java Bytecode Version: unknown", e);
+        }
     }
 }
